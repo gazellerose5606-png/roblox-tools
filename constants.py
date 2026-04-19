f39ba5a304a3e4a7b026484e711ecc33bd78c3fa
g4zelle.dev <gazellerose5606@gmail.com>
2026-04-20 04:15:16 +0900

    update organize constants for clarity

---
+++ constants.py
@@ -0,0 +1,38 @@
+MAX_PLAYERS = 100
+
+DEFAULT_GAME_MODE = 'Classic'
+
+NETWORK_TIMEOUT = 30
+
+ITEM_TYPES = {
+    'weapon': 'Weapon',
+    'armor': 'Armor',
+    'potion': 'Potion'
+}
+
+RANKS = {
+    'guest': 1,
+    'newbie': 2,
+    'advanced': 3,
+    'pro': 4
+}
+
+COLORS = {
+    'red': '#FF0000',
+    'green': '#00FF00',
+    'blue': '#0000FF',
+    'yellow': '#FFFF00'
+}
+
+LEVEL_REQUIREMENTS = {
+    1: 0,
+    2: 100,
+    3: 250,
+    4: 500
+}
+
+ITEM_LIMITS = {
+    'maxWeapons': 5,
+    'maxArmors': 3,
+    'maxPotions': 10
+}
