a336364b4750655bd6adc7e72320ea5d3bd573eb
g4zelle.dev <gazellerose5606@gmail.com>
2026-04-22 04:16:19 +0900

    add type annotations and docstrings

--- logger.py
+++ logger.py
@@ -1,28 +1,34 @@
 import logging
 
+
 class Logger:
-    def __init__(self, name):
+    """Custom logger for Roblox tools."""
+
+    def __init__(self, name: str) -> None:
+        """Initialize the logger with the given name."""
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
         handler = logging.StreamHandler()
-        handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
 
-    def debug(self, message):
+    def debug(self, message: str) -> None:
+        """Log a debug message."""
         self.logger.debug(message)
 
-    def info(self, message):
+    def info(self, message: str) -> None:
+        """Log an info message."""
         self.logger.info(message)
 
-    def warning(self, message):
+    def warning(self, message: str) -> None:
+        """Log a warning message."""
         self.logger.warning(message)
 
-    def error(self, message):
+    def error(self, message: str) -> None:
+        """Log an error message."""
         self.logger.error(message)
 
-    def critical(self, message):
+    def critical(self, message: str) -> None:
+        """Log a critical message."""
         self.logger.critical(message)
-
-logger = Logger(__name__)
