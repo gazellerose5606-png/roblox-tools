84f47c14fe28e1a77b1421a0b8825361f2a81a15
g4zelle.dev <gazellerose5606@gmail.com>
2026-04-26 01:00:16 +0900

    implement retry logic for network operations

--- utils.py
+++ utils.py
@@ -1,36 +1,14 @@
-import json
+import time
 import requests
 
-class RobloxAPI:
-    BASE_URL = 'https://api.roblox.com/'
-
-    @staticmethod
-    def fetch_data(endpoint):
-        response = requests.get(f'{RobloxAPI.BASE_URL}{endpoint}')
-        response.raise_for_status()
-        return response.json()
-
-    @staticmethod
-    def get_user_info(user_id):
-        endpoint = f'users/{user_id}'
-        return RobloxAPI.fetch_data(endpoint)
-
-    @staticmethod
-    def get_game_info(game_id):
-        endpoint = f'games/{game_id}'
-        return RobloxAPI.fetch_data(endpoint)
-
-    @staticmethod
-    def get_asset_info(asset_id):
-        endpoint = f'assets/{asset_id}'
-        return RobloxAPI.fetch_data(endpoint)
-
-    @staticmethod
-    def save_to_json(data, file_name):
-        with open(file_name, 'w') as json_file:
-            json.dump(data, json_file, indent=4)
-
-    @staticmethod
-    def load_from_json(file_name):
-        with open(file_name, 'r') as json_file:
-            return json.load(json_file)
+def retry_request(url, retries=3, backoff=2):
+    for i in range(retries):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            if i < retries - 1:
+                time.sleep(backoff ** i)
+            else:
+                raise e
