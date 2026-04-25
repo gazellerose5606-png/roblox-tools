99e62b95682f009ad61ab65021b16e83d5236c8d
g4zelle.dev <gazellerose5606@gmail.com>
2026-04-26 04:01:54 +0900

    implement logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,11 +1,17 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
 def setup_logger(name, log_file, level=logging.INFO):
     logger = logging.getLogger(name)
-    handler = RotatingFileHandler(log_file, maxBytes=2*1024*1024, backupCount=5)
+    logger.setLevel(level)
+    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=3)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-    logger.setLevel(level)
     return logger
+
+if __name__ == '__main__':
+    log = setup_logger('example_logger', 'example.log')
+    log.info('Logger is set up')
+    log.error('This is an error message')
