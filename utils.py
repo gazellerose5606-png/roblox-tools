9697dc0bc9c2924efe41f52f91a5d9f1a2f31826
g4zelle.dev <gazellerose5606@gmail.com>
2026-04-26 01:02:48 +0900

    update implement roblox data handling utility functions

--- utils.py
+++ utils.py
@@ -1,14 +1,29 @@
-import time
+import json
 import requests
 
-def retry_request(url, retries=3, backoff=2):
-    for i in range(retries):
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            return response.json()
-        except requests.exceptions.RequestException as e:
-            if i < retries - 1:
-                time.sleep(backoff ** i)
-            else:
-                raise e
+class RobloxAPI:
+    BASE_URL = 'https://api.roblox.com/'
+
+    @staticmethod
+    def get_user_info(user_id):
+        response = requests.get(f'{RobloxAPI.BASE_URL}users/{user_id}')
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def get_asset_info(asset_id):
+        response = requests.get(f'{RobloxAPI.BASE_URL}assets/{asset_id}')
+        response.raise_for_status()
+        return response.json()
+
+    @staticmethod
+    def format_data(data):
+        return json.dumps(data, indent=4)
+
+    @staticmethod
+    def save_to_file(data, filename):
+        with open(filename, 'w') as file:
+            file.write(RobloxAPI.format_data(data))
+
+user_info = RobloxAPI.get_user_info(1)
+RobloxAPI.save_to_file(user_info, 'user_info.json')
