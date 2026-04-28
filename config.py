551bec63bdca2305ac0972449a149c6c2461c6b6
Yuki Tanaka <gazellerose5606@gmail.com>
2026-04-29 08:02:28 +0900

    implement configuration loader with defaults

--- config.py
+++ config.py
@@ -1,32 +1,18 @@
 import json
 import os
 
-class ConfigLoader:
-    def __init__(self, default_config_filepath):
-        self.defaults = self.load_json(default_config_filepath)
-        self.config = self.defaults.copy()
+def load_config(filename='config.json', defaults=None):
+    if defaults is None:
+        defaults = {}
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            config = json.load(f)
+        config = {**defaults, **config}
+    else:
+        config = defaults
+    return config
 
-    def load_json(self, filepath):
-        if not os.path.exists(filepath):
-            return {}
-        with open(filepath, 'r') as file:
-            return json.load(file)
-
-    def update(self, custom_config_filepath):
-        custom_config = self.load_json(custom_config_filepath)
-        self.config.update(custom_config)
-
-    def get(self, key, default=None):
-        return self.config.get(key, default)
-
-    def save(self, filepath):
-        with open(filepath, 'w') as file:
-            json.dump(self.config, file, indent=4)
-
-    def reset(self):
-        self.config = self.defaults.copy()
-
-# Usage Example:
-# config_loader = ConfigLoader('default_config.json')
-# config_loader.update('custom_config.json')
-# value = config_loader.get('key', 'default_value')
+if __name__ == '__main__':
+    default_config = {'key1': 'value1', 'key2': 'value2'}
+    config = load_config(defaults=default_config)
+    print(config)
